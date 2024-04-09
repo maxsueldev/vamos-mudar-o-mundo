3cd147768de153126dd7fc880a458daa7d54977e
unknown <maxsuel.dev@gmail.com>
2024-04-09 00:28:42 -0300

    vamos-mudar-o-mundo project

---
+++ enviar.php
@@ -0,0 +1,94 @@
+<?php 
+
+$email_envio = 'maxsuelf94@gmail.com'; //Email receptor
+$email_pass = '##########'; //Senha do email
+
+$site_name = '#####'; //Nome do site
+$site_url = ''; //Url do site
+
+$host_smtp = 'smtp.gmail.com'; //HOST SMTP Ex: smtp.domain.com.br
+$host_port = '587'; //Porta do host
+
+//Variáveis do formulário
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$assunto = $_POST['assunto'];
+$mensagem = $_POST['mensagem'];
+
+//Conteúdo do formulário
+
+$body_content = "De: $nome \n Email: $email \n Assunto: $assunto \n Mensagem: $mensagem";
+
+//Não mudar a partir daqui
+
+// if($_POST['leaveblank'] =! '' or $_POST['dontchange'] =! 'http://') {
+// 	print "<h2 style=\"
+// 	font-size: 1em;
+// 	margin-top: 20%;
+// 	text-align: center;
+// 	font-family: 'Helvetica', 'Arial', 'Sans-serif';
+// 	font-weight: normal;
+// 	color: #1b1b1b;
+// 	\"><center><span>Aconteceu algum erro!</span><p>Você pode tentar novamente ou enviar direto para " . $email_envio . "!</p></center></h2>";
+
+// 	print "<html style=\"background: #fff;\"></html>";
+// 	print "<meta HTTP-EQUIV='Refresh' CONTENT='10;URL=" . $site_url . "'>";
+// }
+
+/* else */  
+
+if(isset($_POST['nome'])) {
+	require('./PHPMailer/PHPMailerAutoload.php');
+
+	$mail = new PHPMailer;
+	$mail->CharSet = 'UTF-8';
+
+	$mail->isSMTP();
+	$mail->Host = $host_smtp;
+	$mail->SMTPAuth = true;
+	$mail->Username = $email_envio;
+	$mail->Password = $email_pass;
+	$mail->Port = $host_port;
+
+	$mail->From = "#########@gmail.com";
+
+	$mail->addAddress("##########@gmail.com");
+
+	$mail->FromName = 'Formulário de Contato';
+	$mail->AddReplyTo($_POST['email'], $_POST['nome']);
+
+	$mail->WordWrap = 70;
+
+	$mail->Subject = 'Formulário - ' . $site_name . ' - ' . $_POST['nome'];
+
+	$mail->Body = $body_content;
+
+	if(!$mail->send()) {
+		print "<h2 style=\"
+		font-size: 1.5em;
+		margin-top: 20%;
+		text-align: center;
+		font-family: 'Inconsolata', 'Arial', 'Sans-serif';
+		font-weight: normal;
+		color: #fdc64b;
+		\"><center><span>Aconteceu algum erro!</span><p>Você pode tentar novamente ou enviar direto para " . "joannaliins@gmail.com" . "!</p></center></h2>";
+
+		print "<html style=\"background: #fff;\"></html>";
+		print "<meta HTTP-EQUIV='Refresh' CONTENT='10;URL=" . $site_url . "'>";
+	} else {
+		print "<h2 style=\"
+		font-size: 1.5em;
+		margin-top: 20%;
+		text-align: center;
+		font-family: 'Inconsolata', 'Arial', 'Sans-serif';
+		font-weight: normal;
+		color: #89bb50;
+		\"><center><span>Formulário enviado!</span><p>Em breve entrarei em contato!</p></center></h2>";
+
+		print "<html style=\"background: #fff;\"></html>";
+		print "<meta HTTP-EQUIV='Refresh' CONTENT='2;URL=" . $site_url . "'>";
+	}
+}
+
+?>
---
+++ js/main.js
@@ -0,0 +1,143 @@
+// // Mudando o header quando rola o scroll //
+
+// $(window).scroll(function() {
+// if ($(this).scrollTop() > 1){  
+//     $('header').addClass("sticky");
+//   }
+//   else{
+//     $('header').removeClass("sticky");
+//   }
+// });
+
+// ///////////////////////////////////////////
+
+$(document).ready(function(){
+	
+	// Site principal e newsllater
+	var form = document.getElementById("form_contato");
+	var send = $("#enviar");
+
+	$(form).submit(function(event){
+  		if (form.checkValidity()) {
+    	send.attr('disabled', 'disabled');
+    	$(send).css("cursor", "wait");
+  		}
+	});
+
+	// Contato do Blog (contato.php)
+	var form_blog = document.getElementById("form_blog");
+	var send_blog = $("#enviar_blog");
+
+	$(form_blog).submit(function(event){
+  		if (form_blog.checkValidity()) {
+    	send_blog.attr('disabled', 'disabled');
+    	$(send_blog).css("cursor", "wait");
+  		}
+	});
+});
+
+// Formulário site principal e newsllater
+
+$('.formphp').on('submit', function() {
+	var emailContato = "joannaliins@gmail.com"; //Escrever o email 
+
+	var that = $(this),
+		url = that.attr('action'),
+		type = that.attr('method'),
+		data = {};
+
+	that.find('[name]').each(function(index, value) {
+		var that = $(this),
+			name = that.attr('name'),
+			value = that.val();
+
+		data[name] = value;
+	});
+
+	$.ajax({
+		url: url,
+		type: type,
+		data: data,
+		success: function(response) {
+
+			if($('[name="leaveblank"]').val().length != 0) {
+				$('.formphp').html("<div id='form-erro'></div>");
+				$('#form-erro').html("<span>Falha no envio! Você pode tentar novamente, ou enviar direto para " + emailContato + " </span>")
+				.hide()
+				.fadeIn(1500, function() {
+					$('#form-erro');
+				});
+			} else {
+				$('.formphp').html("<div id='form-send'></div>");
+				$('#form-send').html("<span>Mensagem enviada! Em breve entrarei em contato!</span>")
+				.hide()
+				.fadeIn(1500, function() {
+					$('#form-send');
+				});
+			};
+		},
+		error: function(response) {
+			$('.formphp').html("<div id='form-erro'></div>");
+				$('#form-erro').html("<span>Falha no envio! Você pode tentar novamente, ou enviar direto para " + emailContato + " </span>")
+				.hide()
+				.fadeIn(1500, function() {
+					$('#form-erro');
+				});
+		}
+	});
+
+	return false;
+});
+
+// Formulário contato Blog (contato.php)
+
+$('.formphp_blog').on('submit', function() {
+	var emailContato = "joannaliins@gmail.com"; //Escrever o email 
+
+	var that = $(this),
+		url = that.attr('action'),
+		type = that.attr('method'),
+		data = {};
+
+	that.find('[name]').each(function(index, value) {
+		var that = $(this),
+			name = that.attr('name'),
+			value = that.val();
+
+		data[name] = value;
+	});
+
+	$.ajax({
+		url: url,
+		type: type,
+		data: data,
+		success: function(response) {
+
+			if($('[name="leaveblank"]').val().length != 0) {
+				$('.formphp_blog').html("<div id='form-erro_blog'></div>");
+				$('#form-erro_blog').html("<span>Falha no envio! Você pode tentar novamente, ou enviar direto para " + emailContato + " </span>")
+				.hide()
+				.fadeIn(1500, function() {
+					$('#form-erro_blog');
+				});
+			} else {
+				$('.formphp_blog').html("<div id='form-send_blog'></div>");
+				$('#form-send_blog').html("<span>Mensagem enviada! Em breve entrarei em contato!</span>")
+				.hide()
+				.fadeIn(1500, function() {
+					$('#form-send_blog');
+				});
+			};
+		},
+		error: function(response) {
+			$('.formphp_blog').html("<div id='form-erro_blog'></div>");
+				$('#form-erro_blog').html("<span>Falha no envio! Você pode tentar novamente, ou enviar direto para " + emailContato + " </span>")
+				.hide()
+				.fadeIn(1500, function() {
+					$('#form-erro_blog');
+				});
+		}
+	});
+
+	return false;
+});
